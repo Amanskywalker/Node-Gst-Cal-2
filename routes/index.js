c5ad6e577d6eaf2b0ca444709ae2b19da4868248
amanskywalker <ak47su30ac@gmail.com>
2018-07-03 04:00:53 +0530

    added route

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,20 @@ const bookshelf = require('bookshelf')(knex);
 router.get('/', function(req, res, next) {
   var itemsList = knex.select('*').from('items')
     .then(function(items){
-      console.dir(items);
+      var count5 = 0, count12 = 0, count18 = 0, count28 = 0;
+      for (var i in items) {
+        if (items[i].slab == 5) {
+          count5++;
+        } else if (items[i].slab == 12) {
+          count12++;
+        } else if (items[i].slab == 18) {
+          count18++;
+        } else if (items[i].slab == 28) {
+          count28++;
+}
+        console.dir(items[i]);
+      }
+      //items.foreach
     });
   //console.dir(itemsList);
   //res.render('index', { title: 'Express' });
