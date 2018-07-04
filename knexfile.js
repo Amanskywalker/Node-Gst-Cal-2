c333948f17d0e4c49c0c03c4e20d4eb2feff1177
amanskywalker <ak47su30ac@gmail.com>
2018-07-04 19:26:56 +0530

    Updated DB config

--- knexfile.js
+++ knexfile.js
@@ -13,11 +13,11 @@ module.exports = {
     client: 'pg',
     version: '10.4',
     connection: {
-      host:     'ec2-54-243-253-24.compute-1.amazonaws.com',
+      host:     'ec2-54-225-230-142.compute-1.amazonaws.com',
       port:     '5432',
-      database: 'dbd0cgtuf7pkpq',
-      user:     'mmnmobalbkgsvt',
-      password: '32debd7086615dce02c47ecb45da4d4f5ce58e8ceb155ff9503510867a3d55d6'
+      database: 'd4lddbu015btv5',
+      user:     'osuhliuihgsrog',
+      password: '591eebe7e4fc5c3c9814e33e2022b987c2e47289eb38aad1c09ba40be11b6d76'
     },
     migrations: {
       tableName: 'knex_migrations'
