17b8bcb9a66092e25ce345dc0b3c52df148349cd
amanskywalker <ak47su30ac@gmail.com>
2018-07-04 18:13:05 +0530

    Added the minimal working application 	modified:   package-lock.json 	modified:   package.json 	modified:   routes/index.js 	modified:   views/index.pug

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,15 @@ var config      = require('../knexfile.js');
 var env         = 'development';
 var knex        = require('knex')(config[env]);
 module.exports = knex;
-const bookshelf = require('bookshelf')(knex);
+const Request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var itemsList = knex.select('*').from('items')
+  knex.select('*').from('items')
     .then(function(items){
-      var count5 = 0, count12 = 0, count18 = 0, count28 = 0;
+      var count =0, count5 = 0, count12 = 0, count18 = 0, count28 = 0;
       for (var i in items) {
+        count++;
         if (items[i].slab == 5) {
           count5++;
         } else if (items[i].slab == 12) {
@@ -20,13 +21,43 @@ router.get('/', function(req, res, next) {
           count18++;
         } else if (items[i].slab == 28) {
           count28++;
-}
-        console.dir(items[i]);
+        };
       }
+      var cd = [count5/count*100, count12/count*100, count18/count*100, count28/count*100];
+      console.log(cd);
+      res.render('index', { title: 'GST Cal', items : items, cd : cd });
       //items.foreach
     });
   //console.dir(itemsList);
-  //res.render('index', { title: 'Express' });
+  //
+});
+
+router.post('/add', function(req, res, next) {
+  var str = '{ "expr": [ "sum = '+ req.body.price +'*'+ req.body.slab +'/100","total = sum + '+ req.body.price +'"], "precision": 14}';
+  Request.post({
+    "headers": { "content-type": "application/json"},
+    "url": "http://api.mathjs.org/v4/",
+    "body": str
+    }, (error, response, body) => {
+      if(error) {
+          return console.log(error);
+      }
+      var mathsAPIResponseBody = JSON.parse(body);
+      console.log(mathsAPIResponseBody.result[0]);
+      console.log(mathsAPIResponseBody.result[1]);
+      knex('items').insert({
+          name: req.body.name,
+          price: req.body.price,
+          slab: req.body.slab,
+          sum: mathsAPIResponseBody.result[0],
+          total: mathsAPIResponseBody.result[1],
+          createdAt: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+          updatedAt: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+          })
+      .then( function (result) {
+          res.redirect('/');
+       })
+    })
 });
 
 module.exports = router;
